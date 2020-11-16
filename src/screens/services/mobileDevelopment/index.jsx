873c4748f37018009cd9e9f8547049100ec56257
Paul <libertypaul4@gmail.com>
2020-11-16 15:46:19 -0500

    creation of slider.jsx and SliderCard.jsx inside components and SliderHidden.jsx inside section in mobileDevelopment

--- src/screens/services/mobileDevelopment/index.jsx
+++ src/screens/services/mobileDevelopment/index.jsx
@@ -2,12 +2,14 @@ import Banner from './components/Banner';
 import ImportanceApplications from './components/ImportanceApplications';
 import ApplicationsAtYourFingertips from './sections/ApplicationsAtYourFingertips';
 import MobileApplicationTypes from './sections/MobileApplicationTypes';
+import SliderHidden from './sections/SliderHidden';
 
 const index = () => (
   <>
     <Banner />
     <ImportanceApplications />
     <MobileApplicationTypes />
+    <SliderHidden />
     <ApplicationsAtYourFingertips />
   </>
 );
