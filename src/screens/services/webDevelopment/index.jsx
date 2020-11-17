42a9f3374a07bb3c506b441a5903669f14e98702
Paul <libertypaul4@gmail.com>
2020-11-17 11:48:26 -0500

    Creation of IncreaseVisibility.jsx and Upgrade.jsx inside Components and CompanyVisiblity.jsx and NeedUpdat.jsx in webDevelopment

--- src/screens/services/webDevelopment/index.jsx
+++ src/screens/services/webDevelopment/index.jsx
@@ -1,8 +1,13 @@
 import WebApplicationTypes from './sections/WebApplicationTypes';
 import PartOfInternetWorld from './sections/PartOfInternetWorld';
+import CompanyVisibility from './sections/CompanyVisibility';
+import NeedUpdate from './sections/NeedUpdate';
+
 
 const index = () => (
   <>
+    <CompanyVisibility />
+    <NeedUpdate />
     <WebApplicationTypes />
     <PartOfInternetWorld />
   </>
