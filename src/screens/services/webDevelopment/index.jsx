dd1ea7dbb79607ce61a0b2f13b254853564f0eff
Paul <libertypaul4@gmail.com>
2020-11-19 10:21:35 -0500

    in webDevelopment the component Questions.jsx and SliderTech.jsx were introduced inside sections

--- src/screens/services/webDevelopment/index.jsx
+++ src/screens/services/webDevelopment/index.jsx
@@ -2,13 +2,17 @@ import WebApplicationTypes from './sections/WebApplicationTypes';
 import PartOfInternetWorld from './sections/PartOfInternetWorld';
 import Banner from './sections/Banner';
 import VisibilityCompany from './sections/VisibilityCompany';
+import SliderTech from './sections/SliderTech';
+import Questions from './sections/Questions';
 
 const index = () => (
   <>
     <Banner />
     <VisibilityCompany />
     <WebApplicationTypes />
+    <SliderTech />
     <PartOfInternetWorld />
+    <Questions />
   </>
 );
 
