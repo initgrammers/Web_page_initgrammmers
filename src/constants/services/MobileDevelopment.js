95051bcc9e92b0f1c0805c30d245d8cdc34fe7ce
Oscar España <espaaoscar@gmail.com>
2020-11-19 12:46:43 -0500

    Create the Web Developmentconstants

---
+++ src/constants/services/MobileDevelopment.js
@@ -0,0 +1,35 @@
+export const MobileTechnologies = [
+  {
+    label: 'Label 1',
+    image: '/assets/svg/Flutter.svg',
+  },
+  {
+    label: 'Label 2',
+    image: '/assets/svg/react-2.svg',
+  },
+  {
+    label: 'Label 3',
+    image: '/assets/svg/android.svg',
+  },
+  {
+    label: 'Label 4',
+    image: '/assets/svg/firebase-1.svg',
+  },
+  {
+    label: 'Label 5',
+    image: '/assets/svg/angular-icon-1.svg',
+  },
+  {
+    label: 'Label 6',
+    image: '/assets/svg/apple.svg',
+  },
+  {
+    label: 'Label 7',
+    image: '/assets/svg/next-js.svg',
+  },
+  {
+    label: 'Label 8',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQunGj_qSj53jScTHWkKAX8WjlSsIuf2CggIw&usqp=CAU',
+  },
+];
