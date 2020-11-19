cdaa6523ce347c7195cb34105b39aab3280c3ad3
Paul <libertypaul4@gmail.com>
2020-11-19 13:49:39 -0500

    transfer of text and images within a sigle MobileDevelopment.js and webDevelopment.js in constants

--- src/constants/services/MobileDevelopment.js
+++ src/constants/services/MobileDevelopment.js
@@ -1,35 +1,86 @@
 export const MobileTechnologies = [
   {
-    label: 'Label 1',
+    label: 'Flutter',
     image: '/assets/svg/Flutter.svg',
   },
   {
-    label: 'Label 2',
+    label: 'React Native',
     image: '/assets/svg/react-2.svg',
   },
   {
-    label: 'Label 3',
+    label: 'Android',
     image: '/assets/svg/android.svg',
   },
   {
-    label: 'Label 4',
+    label: 'Firebase',
     image: '/assets/svg/firebase-1.svg',
   },
   {
-    label: 'Label 5',
+    label: 'Angular',
     image: '/assets/svg/angular-icon-1.svg',
   },
   {
-    label: 'Label 6',
+    label: 'iOS',
     image: '/assets/svg/apple.svg',
   },
   {
-    label: 'Label 7',
+    label: 'Next.js',
     image: '/assets/svg/next-js.svg',
   },
   {
-    label: 'Label 8',
+    label: 'WhatsApp',
     image:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQunGj_qSj53jScTHWkKAX8WjlSsIuf2CggIw&usqp=CAU',
   },
 ];
+
+export const typesApplication = [
+  {
+    title: 'Aplicaciones nativas',
+    description:
+      'El desarrollo de estas aplicaciones móviles va dirigido específicamente para un sistema operativo (iOS, Android),  programada bajo un lenguage específico, la ventaja de estas apps es que aprovechan todas las funcionalidades del dispositivo.',
+  },
+  {
+    title: 'Aplicaciones Web',
+    description:
+      'Básicamente es una página web optimizada de tal manera que se puede adaptar a cualquier dispositivo móvil, está optimización es posible a HTML, CSS y Javascript. Se puede acceder a la aplicación web a través de un navegador web ingresando su URL.',
+  },
+  {
+    title: 'Aplicaciones híbridas',
+    description:
+      'Desarrollo de aplicaciones móviles que se programan para funcionar en varias plataformas iOS o Android. Pueden adaptarse a cualquier dispositivo móvil y aprovechar el acceso a las funcionalidades del dispositivo.Las aplicaciones híbridas permiten ahorrar recursos y tiempo de desarrollo.',
+  },
+  {
+    title: 'Aplicaciones PWA',
+    description:
+      'Desarrollo de aplicaciones móviles que combina aspectos de una aplicación nativa y aplicación web. Se desarrollan en lenguajes de programación web por lo que pueden adaptarse a cualquier dispositivo móvil y aprovechar el acceso a las funcionalidades del dispositivo. ',
+  },
+];
+
+export const questions = [{
+  id: 0,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: ` Tus productos estarían disponibles en todo el mundo, se podrían
+              hacer los pedidos con mucha facilildad y efectividad. Con un
+              excelente servicio desde q usted se conecta con nosostros.`,
+},
+{
+  id: 1,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: `Tus productos estarían disponibles en todo el mundo, se podrían
+              hacer los pedidos con mucha facilildad y efectividad. Con un
+              excelente servicio desde q usted se conecta con nosostros.`,
+},
+{
+  id: 2,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: `Tus productos estarían disponibles en todo el mundo, se podrían
+              hacer los pedidos con mucha facilildad y efectividad. Con un
+              excelente servicio desde q usted se conecta con nosostros.`,
+}, {
+  id: 3,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: `Tus productos estarían disponibles en todo el mundo, se podrían
+              hacer los pedidos con mucha facilildad y efectividad. Con un
+              excelente servicio desde q usted se conecta con nosostros.`,
+}];
---
+++ src/constants/services/webDevelopment.js
@@ -0,0 +1,98 @@
+export const MobileTechnologies = [
+  {
+    label: 'HTML.5',
+    image:
+          '/assets/svg/html-5.svg',
+  },
+  {
+    label: 'Wordpress',
+    image:
+          '/assets/svg/wordpress.svg',
+  },
+  {
+    label: 'CSS',
+    image:
+          '/assets/svg/css-3.svg',
+  },
+  {
+    label: 'Firebase',
+    image:
+          '/assets/svg/firebase-1.svg',
+  },
+  {
+    label: 'Angular',
+    image:
+          '/assets/svg/angular-icon-1.svg',
+  },
+  {
+    label: 'iOS',
+    image:
+          '/assets/svg/apple.svg',
+  },
+  {
+    label: 'Next.js',
+    image:
+          '/assets/svg/next-js.svg',
+  },
+  {
+    label: 'WhatsApp',
+    image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQunGj_qSj53jScTHWkKAX8WjlSsIuf2CggIw&usqp=CAU',
+  },
+];
+
+export const questions = [{
+  id: 0,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: ` Tus productos estarían disponibles en todo el mundo, se podrían
+                hacer los pedidos con mucha facilildad y efectividad. Con un
+                excelente servicio desde q usted se conecta con nosostros.`,
+},
+{
+  id: 1,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: `Tus productos estarían disponibles en todo el mundo, se podrían
+                hacer los pedidos con mucha facilildad y efectividad. Con un
+                excelente servicio desde q usted se conecta con nosostros.`,
+},
+{
+  id: 2,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: `Tus productos estarían disponibles en todo el mundo, se podrían
+                hacer los pedidos con mucha facilildad y efectividad. Con un
+                excelente servicio desde q usted se conecta con nosostros.`,
+}, {
+  id: 3,
+  title: '¿Cómo mejoraría mi produccción?',
+  details: `Tus productos estarían disponibles en todo el mundo, se podrían
+                hacer los pedidos con mucha facilildad y efectividad. Con un
+                excelente servicio desde q usted se conecta con nosostros.`,
+}];
+
+export const typesApplication = [
+  {
+    title: 'Aplicación web estática',
+    description:
+        'Este tipo de aplicación muestra información específica y está planificada para que se realicen los menores cambios posibles. Por lo general se desarrollan en HTML, CSS y JavaScript, y pueden integrar contenido como videos, imágenes animadas, sliders, mapas, etc. Son ideales para proyectos, portafolios e información de tipo estático, lo que permite reducir la cantidad de recursos como tiempo y dinero. ',
+  },
+  {
+    title: ' Aplicación web dinámica',
+    description:
+        'Ofrecen una mayor versatilidad para los usuarios, integran una amplia gama de funcionalidades, adaptándose a las necesidades de los clientes. Tiene la ventaja de poder conectarse a una base de datos donde almacena la información que puede ser consultada en cualquier momento.  Pueden ser gestionadas por cualquier persona debido a que cuenta con un panel de administración denominado CMS (sistema de gestión de contenidos), que permite agregar o modificar contenido, así como entradas de blog, noticias, imágenes, etc.',
+  },
+  {
+    title: 'E-commerce',
+    description:
+        'Es el tipo de aplicación web pensado para tiendas online. Ofrece mayores funcionalidades, porque permite utilizar sistemas para recibir pagos desde tarjetas de crédito, PayPal, efectivo, etc. Además de sincronizarse con la gestión de inventario y logística de envíos podrás controlar desde cualquier parte del mundo la  información de los productos o servicios que tu empresa ofrece.',
+  },
+  {
+    title: 'Portal web app',
+    description:
+        'También conocido como Landing Page, consta de una página principal con varias secciones o apartados, en donde se muestra todo el contenido de la misma. Dicho contenido puede albergar videos, formularios, textos, información de redes sociales, imágenes, etc.',
+  },
+  {
+    title: 'Aplicación web con “Gestor de Contenidos”',
+    description:
+        'Es el tipo de aplicación web pensado para tiendas online. Ofrece mayores funcionalidades, porque permite utilizar sistemas para recibir pagos desde tarjetas de crédito, PayPal, efectivo, etc. Además de sincronizarse con la gestión de inventario y logística de envíos podrás controlar desde cualquier parte del mundo la  información de los productos o servicios que tu empresa ofrece.',
+  },
+];
