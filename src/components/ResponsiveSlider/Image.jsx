c4157f0ac70f6173cc6a85c8ba3f1b14de2d9146
Oscar España <espaaoscar@gmail.com>
2020-11-23 07:39:03 -0500

    Add styles to image slider

--- src/components/ResponsiveSlider/Image.jsx
+++ src/components/ResponsiveSlider/Image.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 
 // TODO responsive image and change to Image component from NextJS
 const Image = ({ image, label }) => (
-  <Box m={2}>
+  <Box m={2} display="flex" flexDirection="column" alignItems="center">
     <img
-      style={{ objectFit: 'cover', maxWidth: 100, maxHeight: 100, width: 'auto' }}
+      style={{
+        objectFit: 'cover',
+        maxWidth: 100,
+        maxHeight: 100,
+        width: 'auto',
+      }}
       src={image}
       alt={label}
     />
