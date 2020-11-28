3f5c7d04e549f03af3a4d84b8ef7f2fd36920028
CrisRonda <cristian.j005@gmail.com>
2020-11-27 22:09:39 -0500

    fix

--- src/layouts/AppBar/index.jsx
+++ src/layouts/AppBar/index.jsx
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
 import CustomMenu from '../../components/Menu/Index';
 
-const Navigation = ({ index }) => <CustomMenu indexMenu={index} />;
+const Navigation = ({ index }) => (
+  <>
+    {' '}
+    <CustomMenu indexMenu={index} />
+  </>
+);
 Navigation.propTypes = {
   index: PropTypes.string,
 };
