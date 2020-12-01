79aa1fa03e5aff5b0ad7338b93f55ff7faf556a7
Oscar España <espaaoscar@gmail.com>
2020-12-01 08:02:21 -0500

    Modify the components to recieve props in the MobileDevelopment page

--- src/constants/LandingPage.js
+++ src/constants/LandingPage.js
@@ -56,3 +56,49 @@ export const partnershipModels = [
       'Comprometidos con ayudarte a alcanzar el éxito, trabajamos como tu equipo de tecnología, desarrollo y/o  programación. ',
   },
 ];
+
+const questions = [
+  {
+    id: 0,
+    title: '¿Puedo recibir pagos a través de mi aplicación móvil?',
+    details:
+      'Si, durante el desarrollo de aplicaciones móviles podemos integrar una pasarela de pagos a tu aplicación móvil para que puedas vender productos o servicios.',
+  },
+  {
+    id: 1,
+    title: '¿A qué tiendas de aplicaciones móviles se puede subir mi producto?',
+    details:
+      'Dependiendo del tipo de aplicación que desees,  el desarrollo de aplicaciones móviles puede ser dirigido para que se pueda desplegar la aplicación móvil en una o todas las tiendas de aplicaciones móviles como Google Play Store, App Store de Apple, entre otras, según los requerimientos que tengas.',
+  },
+  {
+    id: 2,
+    title:
+      '¿Puedo agregar más funcionalidades a mi aplicación después de terminado el desarrollo?',
+    details:
+      'Si, creamos aplicaciones modulares, que permiten que se agreguen nuevas funcionalidades en el futuro, trabajamos con buenas prácticas de código para desarrollar las aplicaciones móviles, lo que te permitirá escalar tu producto en el futuro.',
+  },
+  {
+    id: 3,
+    title: 'Aún  no sé qué tipo de aplicación móvil escoger',
+    details:
+      'No hay problema, contamos con servicio de asesoría y diseño de producto para entender mejor tus necesidades y optimizar tu presupuesto y recursos, así desarrollar la mejor aplicación para solucionar tu problema, contáctanos.',
+  },
+  {
+    id: 4,
+    title: '¿En qué dispositivos se puede utilizar mi aplicación móvil?',
+    details:
+      'Desarrollamos aplicaciones móviles para todos los dispositivos móviles: tablets, celulares, computadoras.',
+  },
+  {
+    id: 5,
+    title: 'Aún  no sé qué tipo de aplicación móvil escoger',
+    details:
+      'No hay problema, contamos con servicio de asesoría y diseño de producto para entender mejor tus necesidades y optimizar tu presupuesto y recursos, así desarrollar la mejor aplicación para solucionar tu problema, contáctanos. ',
+  },
+];
+
+export const FAQLandingPage = {
+  titlePart1: 'Preguntas',
+  titlePart2: 'frecuentes',
+  questions,
+};
