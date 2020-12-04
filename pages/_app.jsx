b868c9bb8815a6f1a62785bc7d1065fbfd3f4db4
Oscar España <espaaoscar@gmail.com>
2020-12-04 07:51:20 -0500

    Fix the images in the Ux UI Design Page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -32,7 +32,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          content="minimum-scale=1, initial-scale=1, width=device-width, viewport-fit=cover, shrink-to-fit=no"
         />
         <link
           rel="preload"
