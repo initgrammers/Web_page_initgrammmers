357da8d2feb63835a1ef6d6fee6331e7b2dd790d
Juan Guana <juanc.guana@gmail.com>
2020-12-04 12:02:28 -0500

    feat: add next nprogress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,16 +3,9 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Router from 'next/router';
-import NProgress from 'nprogress';
+import NextNprogress from 'nextjs-progressbar';
 import theme from '../theme/index';
 import '../src/assets/css/globals.css';
-
-Router.events.on('routeChangeStart', () => {
-  NProgress.start();
-});
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
 
 const NoLayout = ({ children }) => children;
 
@@ -29,6 +22,8 @@ const MyApp = ({ Component, pageProps }) => {
 
   return (
     <>
+      <NextNprogress height={6} color="#a3125b" />
+
       <Head>
         <meta
           name="viewport"
