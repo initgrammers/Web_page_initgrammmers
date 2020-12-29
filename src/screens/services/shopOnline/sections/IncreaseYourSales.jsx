e87f596782c3c430553e09b4377be602e4c02e35
Oscar España <espaaoscar@gmail.com>
2020-12-29 15:10:18 -0500

    Fix the theme import in the IncreaseYourSales section - Shop Online

--- src/screens/services/shopOnline/sections/IncreaseYourSales.jsx
+++ src/screens/services/shopOnline/sections/IncreaseYourSales.jsx
@@ -1,18 +1,21 @@
+import { useTheme } from '@material-ui/core';
 import CallToActionSection from '#Components/CallToActionSection';
 import { increaseYourSalesInOneClick } from '#constants/services/ShopOnline';
-import theme from 'theme';
 
-const UnforgettableExperiences = () => (
-  <CallToActionSection
-    titlePart1={increaseYourSalesInOneClick.titlePart1}
-    titlePart2={increaseYourSalesInOneClick.titlePart2}
-    description={increaseYourSalesInOneClick.description}
-    image={increaseYourSalesInOneClick.image}
-    backgroundImage={increaseYourSalesInOneClick.backgroundImage}
-    heightImage={increaseYourSalesInOneClick.heightImage}
-    imageSizes={increaseYourSalesInOneClick.imageSizes}
-    backgroundColor={theme.palette.primary.fade}
-  />
-);
+const UnforgettableExperiences = () => {
+  const { palette } = useTheme();
+  return (
+    <CallToActionSection
+      titlePart1={increaseYourSalesInOneClick.titlePart1}
+      titlePart2={increaseYourSalesInOneClick.titlePart2}
+      description={increaseYourSalesInOneClick.description}
+      image={increaseYourSalesInOneClick.image}
+      backgroundImage={increaseYourSalesInOneClick.backgroundImage}
+      heightImage={increaseYourSalesInOneClick.heightImage}
+      imageSizes={increaseYourSalesInOneClick.imageSizes}
+      backgroundColor={palette.primary.fade}
+    />
+  );
+};
 
 export default UnforgettableExperiences;
