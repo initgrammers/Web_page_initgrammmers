371442b886edf54e86514ea406bd3b90a01b20ee
Oscar España <espaaoscar@gmail.com>
2021-01-05 09:59:19 -0500

    Add the Cloudinary's images in the SEO page

--- src/screens/services/seo/sections/Oportunities.jsx
+++ src/screens/services/seo/sections/Oportunities.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
 import { useTheme } from '@material-ui/core';
 import CallToActionSection from '#Components/CallToActionSection';
-
-const imageSizes = {
-  xs: {
-    width: 288,
-    height: 235,
-  },
-  sm: {
-    width: 552,
-    height: 449,
-  },
-  md: {
-    width: 544,
-    height: 443,
-  },
-  lg: {
-    width: 608,
-    height: 496,
-  },
-};
+import { oportunitieSEO } from '#constants/services/seo';
 
 const IncreaseClics = () => {
   const { palette } = useTheme();
   return (
     <CallToActionSection
-      titlePart1="Posiciona tu página web, "
-      titlePart2="y genera más oportunidades de negocio"
-      description="Genera más tráfico a tu web, impulsa tu marca y conviertete en un referente del mercado."
-      image="/assets/images/oportunidades-para-negocio.png"
-      backgroundImage="/assets/images/figures/seo/bloque5.png"
-      heightImage={895}
-      imageSizes={imageSizes}
+      titlePart1={oportunitieSEO.titlePart1}
+      titlePart2={oportunitieSEO.titlePart2}
+      description={oportunitieSEO.description}
+      image={oportunitieSEO.image}
+      backgroundImage={oportunitieSEO.backgroundImage}
+      heightImage={oportunitieSEO.heightImage}
+      imageSizes={oportunitieSEO.imageSizes}
       backgroundColor={palette.secondary.fade}
     />
   );
