e3d6d3e05e07430ccf65670501efa24bd02328d8
Oscar España <espaaoscar@gmail.com>
2021-01-25 12:17:12 -0500

    feat: add the id to facebook pixel and google analytics in lib files

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALITICS_ID;
+export const GA_TRACKING_ID = '156967754-2';
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
