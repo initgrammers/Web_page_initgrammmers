a1b99664b178c11f6ab7d5c1bb9e808d0fc35bd7
Oscar España <espaaoscar@gmail.com>
2021-03-06 21:13:13 -0500

    feat: instal and config bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['res.cloudinary.com'],
     path: '/_next/image',
@@ -42,4 +46,4 @@ module.exports = {
       },
     ];
   },
-};
+});
