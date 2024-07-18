64332f4aa9c3127d7168ac3cc23c2aea01073d8b
edisonjt <eddyt1999@gmail.com>
2024-07-18 13:35:39 -0500

    fix: :bug: include typography default styles correctly on theme

--- src/shared/config/MuiThemeProvider/theme.js
+++ src/shared/config/MuiThemeProvider/theme.js
@@ -1,8 +1,8 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { primary, secondary } from '../theme/colors';
-import { typography } from '../theme/typography';
+import typography from '../theme/typography';
 
-let theme = createTheme({
+const baseTheme = createTheme({
   typography,
   palette: {
     primary,
@@ -10,26 +10,34 @@ let theme = createTheme({
   },
 });
 
-theme = responsiveFontSizes(theme); 
+const responsiveTheme = responsiveFontSizes(baseTheme);
 
-theme.typography.h1 = {
-  [theme.breakpoints.down('xs')]: {
-    letterSpacing: '-0.87px',
+const theme = {
+  ...responsiveTheme,
+  typography: {
+    ...responsiveTheme.typography,
+    h1: {
+      ...responsiveTheme.typography.h1,
+      [responsiveTheme.breakpoints.down('xs')]: {
+        letterSpacing: '-0.87px',
+      },
+    },
+    h2: {
+      ...responsiveTheme.typography.h2,
+      fontSize: '2.5rem',
+      [responsiveTheme.breakpoints.down('md')]: {
+        fontWeight: 500,
+        letterSpacing: '0.18px',
+      },
+    },
+    h3: {
+      ...responsiveTheme.typography.h3,
+      fontSize: '2rem',
+      [responsiveTheme.breakpoints.down('md')]: {
+        fontWeight: 400,
+      },
+    },
   },
 };
 
-theme.typography.h2 = {
-  [theme.breakpoints.down('md')]: {
-    fontWeight: 500,
-    letterSpacing: '0.18px',
-  },
-};
-
-
-theme.typography.h3 = {
-  [theme.breakpoints.down('md')]: {
-    fontWeight: 400,
-  },
-};
-
-export default theme; 
+export default theme;
