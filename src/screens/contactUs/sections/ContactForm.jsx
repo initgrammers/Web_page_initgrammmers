4f21763c026927aae08a7480d9185838e78bf539
edisonjt <eddyt1999@gmail.com>
2024-07-31 13:05:10 -0500

    feat: :sparkles: mail sender configuration

--- src/screens/contactUs/sections/ContactForm.jsx
+++ src/screens/contactUs/sections/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, styled, TextField, Typography } from '@mui/material';
 import Image from 'next/image';
 import BackgroundImage from '#Components/BackgroundImage';
 import CustomContainer from '#Components/CustomContainer';
@@ -10,124 +10,194 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = ({
-  backgroundImage = '',
-  heightImage = 0,
-  title = '',
-  description = '',
-  image = '',
+  image = 'https://res.cloudinary.com/initgrammers/image/upload/v1609863472/LandingPage/soporte-adicional-sin-costo.png',
 }) => {
   const { t } = useTranslation();
+
   const validationSchema = Yup.object({
-    nombre: Yup.string().required('El nombre es requerido'),
-    apellido: Yup.string().required('El apellido es requerido'),
+    name: Yup.string().required('El nombre es requerido'),
+    lastname: Yup.string().required('El apellido es requerido'),
     email: Yup.string()
       .email('Email inválido')
       .required('El email es requerido'),
-    telefono: Yup.string().required('El teléfono es requerido'),
-    mensaje: Yup.string().required('El mensaje es requerido'),
+    message: Yup.string().required('El mensaje es requerido'),
   });
+
+  const StyledTextField = styled(TextField)({
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'rgba(255, 255, 255, 0.3)',
+      },
+      '&:hover fieldset': {
+        borderColor: 'white',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'white',
+      },
+      '& .MuiInputBase-input': {
+        color: 'white',
+      },
+      '&.MuiInputBase-root': {
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      },
+    },
+    '& label': {
+      color: 'white',
+      '&.Mui-error': {
+        color: 'white',
+      },
+    },
+    '& .MuiFormLabel-root.Mui-focused': {
+      color: 'white',
+    },
+    '& .MuiFormHelperText-root.Mui-error': {
+      color: 'white',
+    },
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
+    const serviceId = 'service_l4swl99';
+    const templateId = 'template_wiee0tc';
+    const publicKey = 'g-ojnw_r-R4fterOM';
+
+    const templateParams = {
+      name: values.name,
+      lastname: values.lastname,
+      email: values.email,
+      phone: values.phone,
+      message: values.message,
+    };
+
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        resetForm();
+      })
+      .catch((error) => {
+        console.error('Error al enviar el correo', error);
+      });
+  };
+
   return (
     <Box
       component="section"
       position="relative"
-      sx={{ background: primary.light, paddingY: 5 }}
+      sx={{ background: primary.light, paddingY: {lg: 0, xs: 3, md:3} }}
     >
-      <BackgroundImage image={backgroundImage} height={heightImage} />
       <CustomContainer>
         <Box
           display="flex"
-          flexDirection="column"
+          flexDirection="row-reverse"
+          gap={3}
           justifyContent="center"
           alignContent="center"
-          width="100%"
-          gap={3}
         >
-          <Box display='flex' justifyContent='center'>
-            <Typography variant="h3" sx={styles.title}>
-              Déjanos tu contacto
-            </Typography>
-          </Box>
-          <Box>
-            <Formik
-              initialValues={{
-                nombre: '',
-                apellido: '',
-                email: '',
-                telefono: '',
-                mensaje: '',
-              }}
-              // validationSchema={validationSchema}
-              onSubmit={(values, { resetForm }) => {
-                console.log(values);
-                resetForm();
-              }}
-            >
-              {({ errors, touched }) => (
-                <Form>
-                  <Box
-                    sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
-                  >
-                    <Field
-                      name="nombre"
-                      as={TextField}
-                      label="Nombre"
-                      variant="outlined"
-                      fullWidth
-                      error={touched.nombre && Boolean(errors.nombre)}
-                      helperText={touched.nombre && errors.nombre}
-                    />
-                    <Field
-                      name="apellido"
-                      as={TextField}
-                      label="Apellido"
-                      variant="outlined"
-                      fullWidth
-                      error={touched.apellido && Boolean(errors.apellido)}
-                      helperText={touched.apellido && errors.apellido}
-                    />
-                    <Field
-                      name="email"
-                      as={TextField}
-                      label="Email"
-                      variant="outlined"
-                      fullWidth
-                      error={touched.email && Boolean(errors.email)}
-                      helperText={touched.email && errors.email}
-                    />
-                    <Field
-                      name="telefono"
-                      as={TextField}
-                      label="Teléfono"
-                      variant="outlined"
-                      fullWidth
-                      error={touched.telefono && Boolean(errors.telefono)}
-                      helperText={touched.telefono && errors.telefono}
-                    />
-                    <Field
-                      name="mensaje"
-                      as={TextField}
-                      label="Mensaje"
-                      variant="outlined"
-                      multiline
-                      rows={4}
-                      fullWidth
-                      error={touched.mensaje && Boolean(errors.mensaje)}
-                      helperText={touched.mensaje && errors.mensaje}
-                    />
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      color='secondary'
-                      sx={{ alignSelf: 'center', width: '200px' }}
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignContent="center"
+            width={{ xs: 468, lg: 504 }}
+            gap={3}
+            flex={1}
+          >
+            <Box display="flex" justifyContent="center">
+              <Typography variant="h3" sx={styles.title}>
+                Déjanos tu contacto
+              </Typography>
+            </Box>
+            <Box>
+              <Formik
+                initialValues={{
+                  name: '',
+                  lastname: '',
+                  email: '',
+                  phone: '',
+                  message: '',
+                }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <Box
+                      sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
                     >
-                      Enviar
-                    </Button>
-                  </Box>
-                </Form>
-              )}
-            </Formik>
+                      <Field
+                        name="name"
+                        as={StyledTextField}
+                        label="Nombre"
+                        variant="outlined"
+                        fullWidth
+                        error={touched.name && Boolean(errors.name)}
+                        helperText={touched.name && errors.name}
+                      />
+                      <Field
+                        name="lastname"
+                        as={StyledTextField}
+                        label="Apellido"
+                        variant="outlined"
+                        fullWidth
+                        error={touched.lastname && Boolean(errors.lastname)}
+                        helperText={touched.lastname && errors.lastname}
+                      />
+                      <Field
+                        name="email"
+                        as={StyledTextField}
+                        label="Email"
+                        variant="outlined"
+                        fullWidth
+                        error={touched.email && Boolean(errors.email)}
+                        helperText={touched.email && errors.email}
+                      />
+                      <Field
+                        name="phone"
+                        as={StyledTextField}
+                        label="Teléfono"
+                        variant="outlined"
+                        fullWidth
+                        error={touched.phone && Boolean(errors.phone)}
+                        helperText={touched.phone && errors.phone}
+                      />
+                      <Field
+                        name="message"
+                        as={StyledTextField}
+                        label="Mensaje"
+                        variant="outlined"
+                        multiline
+                        rows={4}
+                        fullWidth
+                        error={touched.message && Boolean(errors.message)}
+                        helperText={touched.message && errors.message}
+                      />
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="secondary"
+                        sx={{ alignSelf: 'center', width: '200px' }}
+                      >
+                        Enviar
+                      </Button>
+                    </Box>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          </Box>
+          <Box sx={styles.containerImage}>
+            <Box component="figure" sx={styles.image}>
+              <Image
+                quality={100}
+                priority
+                src={image}
+                fill
+                style={styles.mainImage}
+                alt={'Contáctanos'}
+              />
+            </Box>
           </Box>
         </Box>
       </CustomContainer>
