3d0594a742ca5d9f4cb9d5caade18d9d0ff57327
Freddyz5 <freddyltacuri@gmail.com>
2024-08-14 10:49:45 -0500

    feat: :sparkles: Add redirection to new page products and fix redirection portfolio

---
+++ app/[locale]/products/page.jsx
@@ -0,0 +1,10 @@
+import Index from '#screens/products/Index';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <Index />
+  );
+};
+
+export default Page;
---
+++ src/screens/products/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      index Products
+    </div>
+  );
+};
+
+export default Index;
